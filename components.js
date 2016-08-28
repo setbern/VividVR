5aac51fd6559015deb563872c95f9b3bf9f1628a
daryloregan <daryl.oreagan@gmail.com>
2016-08-27 17:51:08 -0700

    blah

--- components.js
+++ components.js
@@ -1,1 +1,10 @@
-javascript components! 
+AFRAME.registerComponent('fireballDisappear', {
+  init: function () {
+    var el = this.el;
+    var fb = document.getElementById('fireball');
+
+    el.addEventListener('click', function () {
+      fb.setAttribute('visible', false);
+    })
+  }
+});
