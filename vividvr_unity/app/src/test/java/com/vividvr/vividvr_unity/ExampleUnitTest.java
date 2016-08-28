aef695dfcd007e23ca00d47faa1109234927cfce
Abhipray Sahoo <abhiprays@gmail.com>
2016-08-27 22:10:30 -0700

    Convex Hull based number of fingers recognizer -- from h3ct0r

---
+++ vividvr_unity/app/src/test/java/com/vividvr/vividvr_unity/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.vividvr.vividvr_unity;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
